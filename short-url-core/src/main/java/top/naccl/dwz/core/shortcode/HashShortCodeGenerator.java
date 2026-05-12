04041518a3220b582557240fcccb7b6f36bd3be2
Admin <3324926481@qq.com>
2026-05-12 21:18:10 +0800

    fix: Bloom过滤器不作为硬性限制，避免相同URL耗尽重试次数

--- short-url-core/src/main/java/top/naccl/dwz/core/shortcode/HashShortCodeGenerator.java
+++ short-url-core/src/main/java/top/naccl/dwz/core/shortcode/HashShortCodeGenerator.java
@@ -4,8 +4,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import top.naccl.dwz.common.constant.ShortUrlConstants;
-import top.naccl.dwz.common.exception.ApiException;
-import top.naccl.dwz.common.enums.ResultCode;
 import top.naccl.dwz.common.util.HashUtils;
 import top.naccl.dwz.core.bloom.RedisBloomFilter;
 
@@ -20,14 +18,11 @@ public class HashShortCodeGenerator implements ShortCodeGenerator {
     }
 
     private String generateWithRetry(String longUrl, int retryCount) {
-        if (retryCount > ShortUrlConstants.MAX_RETRY) {
-            throw new ApiException(ResultCode.ERROR.getCode(), "短码生成冲突过多，请稍后重试");
-        }
         String salt = retryCount > 0 ? String.valueOf(retryCount) : "";
         String hash = HashUtils.murmurHash32ToBase62(longUrl + salt);
         String shortCode = StringUtils.leftPad(hash, ShortUrlConstants.TARGET_LENGTH, '0');
 
-        if (bloomFilter.mightContain(shortCode)) {
+        if (retryCount < ShortUrlConstants.MAX_RETRY && bloomFilter.mightContain(shortCode)) {
             return generateWithRetry(longUrl, retryCount + 1);
         }
         return shortCode;
